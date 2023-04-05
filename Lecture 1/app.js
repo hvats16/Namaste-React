afc4417083328a29a588e3107f39bbe93ad530bc
Harshit Vats <hvats16@gmail.com>
2023-04-05 22:30:41 +0530

    Lecture 1

---
+++ Lecture 1/app.js
@@ -0,0 +1,24 @@
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+  },
+  "Heading 2"
+);
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+  },
+  "Heading 2"
+);
+const conatainer = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
+console.log(heading1);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(conatainer);

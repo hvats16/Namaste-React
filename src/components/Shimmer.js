d4c9ba11a8144231484b7cfcd0addf91fb8cc772
Harshit Vats <hvats16@gmail.com>
2023-06-29 11:05:22 +0530

    Shimmer UI added

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import "../../index.css";
 
 const Shimmer = () => {
-  return (
-    <>
-        <h1>Loading....</h1>
-    </>
-  )
-}
+	return (
+		<>
+			<div className="container">
+				<div className="col-sm-6 col-md-3">
+					<div className="movie--isloading">
+						<div className="loading-image"></div>
+						<div className="loading-content">
+							<div className="loading-text-container">
+								<div className="loading-main-text"></div>
+								<div className="loading-sub-text"></div>
+							</div>
+							<div className="loading-btn"></div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6 col-md-3">
+					<div className="movie--isloading">
+						<div className="loading-image"></div>
+						<div className="loading-content">
+							<div className="loading-text-container">
+								<div className="loading-main-text"></div>
+								<div className="loading-sub-text"></div>
+							</div>
+							<div className="loading-btn"></div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6 col-md-3">
+					<div className="movie--isloading">
+						<div className="loading-image"></div>
+						<div className="loading-content">
+							<div className="loading-text-container">
+								<div className="loading-main-text"></div>
+								<div className="loading-sub-text"></div>
+							</div>
+							<div className="loading-btn"></div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6 col-md-3">
+					<div className="movie--isloading">
+						<div className="loading-image"></div>
+						<div className="loading-content">
+							<div className="loading-text-container">
+								<div className="loading-main-text"></div>
+								<div className="loading-sub-text"></div>
+							</div>
+							<div className="loading-btn"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default Shimmer
+export default Shimmer;
